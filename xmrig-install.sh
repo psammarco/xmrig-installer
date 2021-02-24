8ac433118b75606ad535ff3e73dd8a64f57a2cd1
Pietro Sammarco <30707053+psammarco@users.noreply.github.com>
2021-02-24 22:07:36 +0000

    Update xmrig-install.sh
    Switched to linux-x64 binary from Ubuntu binary due to dependency issues between distros.

--- xmrig-install.sh
+++ xmrig-install.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# This script installs the latest version of xmrig for Debian/Ubuntu availabe at xmrig's github repo.
+# Updates latest version of the xmrig miner for Linux x64 availabe on xmrig's github repo.
 #
 # Author: Pietro Sammarco 
-#
-# 01.16.2020
-# - Fixed version output length  
-# - Added pkill at the end of the update process in order for the "cronjbob" to execute the new installed version 
-# Note: cronjob in question: https://gist.github.com/psammarco/0400f07cb5a8f697a8ae4195bcccf64f
 
 workdir="/tmp"
 
@@ -28,17 +23,17 @@ else
 	cd $workdir
 curl -s https://github.com/xmrig/xmrig/releases/latest \
 	|cut -d '"' -f 2  |xargs curl -s \
-	| grep "focal-x64.tar.gz"  | cut -d '"'  -f 2 |tail -2 |head -1 > url.txt
+	| grep "linux-x64.tar.gz"  | cut -d '"'  -f 2 |tail -2 |head -1 > url.txt
                   echo -n "https://github.com" |cat - url.txt > tmp && mv tmp url.txt 
 		  cat url.txt |xargs wget
 fi
 
-if ls |grep -q focal-x64.tar.gz; then
+if ls |grep -q linux-x64.tar.gz; then
       mv $local/bin $local/backup-bin/bin-$(date "+%m.%d.%Y-%H.%M");
-      tar -C $local -zxvf $workdir/*focal-x64.tar.gz;
+      tar -C $local -zxvf $workdir/*linux-x64.tar.gz;
       mv $local/xmrig-* $local/bin;
       mv version.git $local/bin/version;
-      rm url.txt *focal-x64.tar.gz* ;
+      rm url.txt *linux-x64.tar.gz* ;
       pkill -f xmrig 
       echo "Latest release has been installed!"
 else 
